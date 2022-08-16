e9fb0272b7f1f47499e1e5cc67a7366173ec22ab
gitkvhub <103826383+gitkvhub@users.noreply.github.com>
2022-08-16 13:28:51 +0700

    Update JSWA.js

--- assets/js/JSWA.js
+++ assets/js/JSWA.js
@@ -1,12 +1,14 @@
 function gotowhatsapp() {
     
     var name = document.getElementById("name").value;
+    var lastname = document.getElementById("lastname").value;
     var phone = document.getElementById("phone").value;
     var email = document.getElementById("email").value;
     var service = document.getElementById("service").value;
 
     var url = "https://wa.me/6281906417979?text=" 
-    + "Name: " + name + "%0A"
+    + "Name: " + name + "%20"
+    + lastname + "%0A"
     + "Phone: " + phone + "%0A"
     + "Email: " + email  + "%0A"
     + "Service: " + service; 
