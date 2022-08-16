2ae91afb64121d419caafb6cee31b3f837e1b65b
gitkvhub <103826383+gitkvhub@users.noreply.github.com>
2022-08-16 17:39:04 +0700

    Update jswa.js

--- assets/js/jswa.js
+++ assets/js/jswa.js
@@ -7,7 +7,7 @@ function gotowhatsapp() {
     var service = document.getElementById("service").value;
      var tfmethode = document.getElementById("tfmethode").value;
 
-    var url = "https://wa.me/6281906417979?text=" 
+    var url = "https://wa.me/6281906417979?text=Form%20Pesanan%0A" 
     + "Name: " + name + "%20"
     + lastname + "%0A"
     + "Phone: " + phone + "%0A"
